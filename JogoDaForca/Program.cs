7c6ea8ffafc38195316726cffb31b8fc48abb38b
CaioLLima <caiololima2@gmail.com>
2025-03-25 19:38:42 -0300

    Add project files.

---
+++ JogoDaForca/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace JogoDaForca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string palavraSecreta = "MELANCIA";
+                char[] letrasEncontradas = new char[palavraSecreta.Length];
+
+                for (int i = 0; i < letrasEncontradas.Length; i++)
+                {
+                    letrasEncontradas[i] = '_';
+                }
+                Console.WriteLine("Jogo da Forca.");
+                Console.WriteLine($"A palavra secreta é: {palavraSecreta}.");
+
+                Console.Write("Digite uma letra: ");
+                char chute = Console.ReadLine()[0];
+            }
+            
+        }
+    }
+}

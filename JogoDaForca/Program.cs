3b684d8d95a531ba7d3207c7d9ff0eaba949f9ef
CaioLLima <caiololima2@gmail.com>
2025-03-25 21:01:53 -0300

    Adicionado saída em caso de  vitória e derrota

--- JogoDaForca/Program.cs
+++ JogoDaForca/Program.cs
@@ -14,25 +14,69 @@
                     letrasEncontradas[i] = '_';
                 }
 
-                string dicaDaPalavra = String.Join(" ", letrasEncontradas);
-                Console.WriteLine("Jogo da Forca.");
-                Console.WriteLine($"A palavra secreta é: {dicaDaPalavra}.");
 
-                Console.Write("Digite uma letra: ");
-                char chute = Console.ReadLine()[0];
 
-                for (int i = 0; i < palavraSecreta.Length; i++)
+                bool acertou = false;
+                bool enforcou = false;
+                int qntErros = 0;
+
+                do
                 {
-                    if (palavraSecreta[i] == chute)
-                    {
-                        letrasEncontradas[i] = chute;
-                    }
-                    Console.WriteLine(letrasEncontradas[i]);
-                }
+                    string dicaDaPalavra = String.Join("", letrasEncontradas);
 
-               
+                    Console.Clear();
+                    Console.WriteLine("Jogo da Forca.");
+                    Console.WriteLine($"A quantidade de erros é: {qntErros}.");
+                    Console.WriteLine($"A palavra secreta é: {String.Join("", letrasEncontradas)}.");
+
+                    Console.Write("Digite uma letra: ");
+                    char chute = Console.ReadLine()[0];
+
+                    bool letraFoiEncontrada = false;
+
+                    for (int i = 0; i < palavraSecreta.Length; i++)
+                    {
+                        char letraAtual = palavraSecreta[i];
+                        if (chute == letraAtual)
+                        {
+                            letrasEncontradas[i] = letraAtual;
+                            letraFoiEncontrada = true;
+                        }
+
+
+                        dicaDaPalavra = String.Join("", letrasEncontradas);
+                        Console.Write(letrasEncontradas[i]);
+                    }
+
+                    if (letraFoiEncontrada == false)
+                    {
+                        qntErros++;
+                        Console.WriteLine($"\nVocê errou pela {qntErros}ª vez.");
+                    }
+                    acertou = dicaDaPalavra == palavraSecreta;
+                    enforcou = qntErros > 5;
+
+                    if (acertou)
+                    {
+                        Console.WriteLine("Parabéns, você acertou a palavra secreta!");
+                    }
+                    else if (enforcou)
+                    {
+                        Console.WriteLine($"Você foi enforcado! A palavra secreta era {palavraSecreta}");
+                    }
+
+                }
+                while (acertou == false && enforcou == false);
+
+                Console.ReadLine();
+
+
+
+
+
+
             }
-            
+
         }
     }
 }
